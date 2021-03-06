1dae5cad048d77995778de36a77ac5012b87d637
msgparser <80162840+msgparser@users.noreply.github.com>
2021-03-06 10:48:00 -0500

    Update AWS_Subdomain.sh

--- smallset/AWS_Subdomain.sh
+++ smallset/AWS_Subdomain.sh
@@ -1,12 +1,17 @@
+# Change following HOST to test the service from localhost, EC2 and EKS
+# Service Unit Test
+
+export SERVICE_HOST="localhost"
 for M in $(ls *.msg)
 do
    echo "======[ $M ]===================="
    DATA="'$(cat $M)'"
    curl --request POST\
         --header 'Content-Type: text/plain'\
-          http://msgparser.hemantrumde.com:4000/rawmsg \
+          http://${SERVICE_HOST}:4000/rawmsg \
          -d "$DATA"
    echo
    echo
 done
+
 
